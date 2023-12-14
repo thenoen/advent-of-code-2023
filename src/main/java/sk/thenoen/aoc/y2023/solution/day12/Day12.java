6e8dba13067f863114046479d5b560fb25df9311
marek <kordikmarek@gmail.com>
2023-12-14 08:35:16 +0100

    day 12 - part 2 - attempts

--- src/main/java/sk/thenoen/aoc/y2023/solution/day12/Day12.java
+++ src/main/java/sk/thenoen/aoc/y2023/solution/day12/Day12.java
@@ -1,9 +1,17 @@
 package sk.thenoen.aoc.y2023.solution.day12;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import sk.thenoen.aoc.y2023.solution.Utils;
 
@@ -49,7 +57,7 @@ public class Day12 {
 		return sum;
 	}
 
-	public long solvePart2(String inputPath) { // just for curiosity, too CPU and memory intensive
+	public long solvePart2Bad(String inputPath) { // just for curiosity, too CPU and memory intensive
 
 		final ArrayList<String> lines = Utils.loadLines(inputPath);
 
@@ -58,8 +66,8 @@ public class Day12 {
 
 			final String[] split = line.split(" ");
 			String springsPart = split[0];
-			final String springs = unfold(springsPart, "?");
-			String groupNumbersString = unfold(split[1], ",");
+			final String springs = unfold(springsPart, "?", 5);
+			String groupNumbersString = unfold(split[1], ",", 5);
 
 			final String regex = buildRegex(groupNumbersString);
 
@@ -90,9 +98,111 @@ public class Day12 {
 		return sum;
 	}
 
-	private static String unfold(String springsPart, String delimiter) {
+	public long solvePart2(String inputPath, int unfoldingMultiplier) throws ExecutionException, InterruptedException {
+
+		final Map<Long, Long> longLongMap = parseGroupSizes("##..###.####?#...");
+
+		final ArrayList<String> lines = Utils.loadLines(inputPath);
+
+		long sum = 0;
+		List<Future<Long>> futures = new ArrayList<>();
+
+		try (ExecutorService executorService = Executors.newFixedThreadPool(11)) {
+			for (String line : lines) {
+
+				final String[] split = line.split(" ");
+				String springsPart = split[0];
+				final String springs = unfold(springsPart, "?", unfoldingMultiplier);
+				String groupNumbersString = unfold(split[1], ",", unfoldingMultiplier);
+
+				final Condition condition = buildCondition(groupNumbersString);
+
+				long index = lines.indexOf(line) + 1;
+
+				//				final long lineCount = verifyPermutations(springs, condition);
+
+				final Future<Long> future = executorService.submit(() -> {
+					System.out.println("Line: " + index + " of " + lines.size() + " ... ");
+					return verifyPermutations(springs, condition);
+				});
+				futures.add(future);
+
+				//				System.out.println("\tcount: " + lineCount);
+				//				sum += lineCount;
+			}
+
+		}
+
+		for (Future<Long> future : futures) {
+			sum += future.get();
+		}
+
+		return sum;
+	}
+
+	private static long verifyPermutations(String springs, Condition condition) {
+
+		final Map<Character, Long> typeCounts = getTypeCounts(springs);
+
+		// more gears than needed
+		final Long currentGearCount = typeCounts.getOrDefault('#', 0L);
+		if (currentGearCount > condition.gearsCount) {
+			return 0;
+		}
+
+		// not enough placeholders
+		final long availablePlaceholdersCount = typeCounts.getOrDefault('?', 0L);
+		if ((availablePlaceholdersCount + currentGearCount) < condition.gearsCount) {
+			return 0;
+		}
+
+		final Map<Long, Long> currentGroupSizes = parseGroupSizes(springs);
+		//		for (int i = 0; i < condition.groupSizes.size(); i++) {
+		//			if (currentGroupSizes.get((long) i) == null) {
+		//				break; //continue for next recursion (possible optimization)
+		//			}
+		//			if (currentGroupSizes.get((long) i) > condition.groupSizes.get(i)) {
+		//				return 0;
+		//			}
+		//		}
+
+		for (Long l : currentGroupSizes.keySet()) {
+			if (!currentGroupSizes.get(l).equals(condition.groupSizes.get(l.intValue()))) {
+				return 0;
+			}
+		}
+
+		if (springs.contains("?")) {
+			long sum = 0;
+			sum += verifyPermutations(springs.replaceFirst("\\?", "."), condition);
+			sum += verifyPermutations(springs.replaceFirst("\\?", "#"), condition);
+			//			System.out.println("\t... sum: " + sum);
+			return sum;
+		} else {
+			if (condition.pattern.matcher(springs).matches()) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+	private static long getTypeCount(String springs, char type) {
+		final long currentGearCount = springs.chars()
+											 .filter(c -> c == type)
+											 .count();
+		return currentGearCount;
+	}
+
+	private static Map<Character, Long> getTypeCounts(String springs) {
+		final Map<Character, Long> countGroups = springs.chars()
+														.mapToObj(i -> (char) i)
+														.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		return countGroups;
+	}
+
+	private static String unfold(String springsPart, String delimiter, int times) {
 		List<String> springsParts = new ArrayList<>();
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < times; i++) {
 			springsParts.add(springsPart);
 		}
 		String springs = String.join(delimiter, springsParts);
@@ -117,11 +227,74 @@ public class Day12 {
 		Scanner groupScanner = new Scanner(groupNumbersString);
 		groupScanner.useDelimiter(",");
 		List<String> regexParts = new ArrayList<>();
+		long countOfGears = 0;
 		while (groupScanner.hasNextLong()) {
 			long group = groupScanner.nextLong();
+			countOfGears += group;
 			regexParts.add("#{" + group + "}");
 		}
 		final String regex = "\\.*?" + String.join("\\.+?", regexParts) + "\\.*?";
 		return regex;
+	}
+
+	private static Condition buildCondition(String groupNumbersString) {
+		Scanner groupScanner = new Scanner(groupNumbersString);
+		groupScanner.useDelimiter(",");
+		List<String> regexParts = new ArrayList<>();
+		long countOfGears = 0;
+		List<Long> groupSizes = new ArrayList<>();
+		while (groupScanner.hasNextLong()) {
+			long group = groupScanner.nextLong();
+			countOfGears += group;
+			groupSizes.add(group);
+			regexParts.add("#{" + group + "}");
+		}
+		final String regex = "\\.*?" + String.join("\\.+?", regexParts) + "\\.*?";
+		return new Condition(countOfGears, Pattern.compile(regex), groupSizes);
+	}
+
+	private record Condition(long gearsCount,
+							 Pattern pattern,
+							 List<Long> groupSizes) {
+
+	}
+
+	private static Map<Long, Long> parseGroupSizes(String springs) {
+		Map<Long, Long> groupSizes = new HashMap<>(100);
+		long groupIndex = -1;
+		long currentGroupSize = 0;
+
+		boolean isGear = false;
+
+		final char[] springsAsChars = springs.toCharArray();
+
+		for (int i = 0; i < springsAsChars.length; i++) {
+
+			final char curentChar = springsAsChars[i];
+			if (!isGear && curentChar == '#') {
+				isGear = true;
+				groupIndex++;
+				currentGroupSize++;
+				//				groupSizes.put(groupIndex, currentGroupSize);
+			} else if (curentChar == '#') {
+				currentGroupSize++;
+				//				groupSizes.put(groupIndex, currentGroupSize);
+			} else if (curentChar == '.') {
+				if (isGear) {
+					groupSizes.put(groupIndex, currentGroupSize);
+				}
+				isGear = false;
+				currentGroupSize = 0;
+			} else if (curentChar == '?') {
+				//				groupSizes.put(groupIndex, currentGroupSize);
+				if (isGear) {
+					groupSizes.remove(groupIndex);
+				}
+				return groupSizes;
+			}
+
+		}
+
+		return groupSizes;
 	}
 }
--- src/test/java/sk/thenoen/aoc/y2023/solution/day12/Day12Test.java
+++ src/test/java/sk/thenoen/aoc/y2023/solution/day12/Day12Test.java
@@ -1,6 +1,9 @@
 package sk.thenoen.aoc.y2023.solution.day12;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,15 +24,45 @@ class Day12Test {
 		assertEquals(7090, solution);
 	}
 
+	@Disabled
 	@Test
-	void solvePart2Sample() {
-		final long solution = day12.solvePart2("day12/sample.txt");
+	void solvePart2SampleBad() {
+		final long solution = day12.solvePart2Bad("day12/sample.txt");
+		assertEquals(525152, solution);
+	}
+
+	@Disabled
+	@Test
+	void solvePart2Bad() {
+		final long solution = day12.solvePart2Bad("day12/input.txt");
+		System.out.println("Solution: " + solution);
+		assertEquals(0, solution);
+	}
+
+
+
+	@Test
+	void solvePart1SampleSolution2() throws ExecutionException, InterruptedException {
+		final long solution = day12.solvePart2("day12/sample.txt", 1);
+		assertEquals(21, solution);
+	}
+
+	@Test
+	void solvePart1Solution2() throws ExecutionException, InterruptedException {
+		final long solution = day12.solvePart2("day12/input.txt", 1);
+		System.out.println("Solution: " + solution);
+		assertEquals(7090, solution);
+	}
+
+	@Test
+	void solvePart2Sample() throws ExecutionException, InterruptedException {
+		final long solution = day12.solvePart2("day12/sample.txt", 5);
 		assertEquals(525152, solution);
 	}
 
 	@Test
-	void solvePart2() {
-		final long solution = day12.solvePart2("day12/input.txt");
+	void solvePart2() throws ExecutionException, InterruptedException {
+		final long solution = day12.solvePart2("day12/input.txt", 5);
 		System.out.println("Solution: " + solution);
 		assertEquals(0, solution);
 	}
