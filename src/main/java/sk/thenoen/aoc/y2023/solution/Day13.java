1c79fda5cca2e472a65bed70ff0da8c075de826e
marek <kordikmarek@gmail.com>
2023-12-18 01:07:05 +0100

    day 13 - part 2 - solved

--- src/main/java/sk/thenoen/aoc/y2023/solution/Day13.java
+++ src/main/java/sk/thenoen/aoc/y2023/solution/Day13.java
@@ -33,6 +33,51 @@ public class Day13 {
 		return sum;
 	}
 
+	public long solvePart2(String inputPath) {
+
+		final ArrayList<String> lines = Utils.loadLines(inputPath);
+
+		List<List<String>> patterns = new ArrayList<>();
+
+		long sum = 0;
+
+		List<String> patternLines = new ArrayList<>();
+		for (String line : lines) {
+			if (!line.isBlank()) {
+				patternLines.add(line);
+			} else {
+				patterns.add(patternLines);
+				patternLines = new ArrayList<>();
+			}
+		}
+		patterns.add(patternLines);
+
+		for (List<String> pattern : patterns) {
+			pattern.forEach(System.out::println);
+			System.out.println();
+			sum += calculateAlternativeReflection(pattern);
+		}
+
+		return sum;
+	}
+
+	private static long calculateAlternativeReflection(List<String> patternLines) {
+		final char[][] mapPattern = patternLines.stream()
+												.map(String::toCharArray)
+												.toList()
+												.toArray(char[][]::new);
+
+		int verticalReflection = findVerticalReflection(mapPattern);
+		int horizontalReflection = findHorizontalReflection(mapPattern);
+
+		int alternativeVerticalReflection = findAlternativeVerticalReflection(mapPattern, verticalReflection);
+		int alternativeHorizontalReflection = findAlternativeHorizontalReflection(mapPattern, horizontalReflection);
+
+		System.out.println();
+		final int sum = alternativeVerticalReflection + (alternativeHorizontalReflection * 100);
+		return sum;
+	}
+
 	private static long calculateReflection(List<String> patternLines) {
 		final char[][] mapPattern = patternLines.stream()
 												.map(String::toCharArray)
@@ -40,108 +85,183 @@ public class Day13 {
 												.toArray(char[][]::new);
 
 		//find vertical reflection
-		int verticalReflection = 0;
-		for (int x = 0; x < mapPattern[0].length - 1; x++) {
-			final boolean same = compareColumns(mapPattern, x, x + 1);
-			if (same) {
-				verticalReflection = x + 1;
-				if (x + 1 < mapPattern[0].length - x) {
-					// first half
-					boolean allReflect = true;
-					for (int i = 0; i < x; i++) {
-						if (!compareColumns(mapPattern, i, 2 * x - i + 1)) {
-							allReflect = false;
-							verticalReflection = 0;
-							break;
-						}
-					}
-
-					if(allReflect) {
-						break;
-					}
-				} else {
-					// second half
-					boolean allReflect = true;
-					for (int i = mapPattern[0].length - 1; i > x + 1; i--) {
-						if (!compareColumns(mapPattern, i, 2 * x - i + 1)) {
-							allReflect = false;
-							verticalReflection = 0;
-							break;
-						}
-					}
-
-					if(allReflect) {
-						break;
-					}
-				}
-				System.out.println("vertical reflection found at x: " + verticalReflection);
-			}
-		}
+		int verticalReflection = findVerticalReflection(mapPattern);
 
 		//find horizontal reflection
-		int horizontalReflection = 0;
-		for (int y = 0; y < mapPattern.length - 1; y++) {
-			final boolean same = compareRows(mapPattern, y, y + 1);
-
-			if (same) {
-				horizontalReflection = y + 1;
-				if (y + 1 < mapPattern.length - y) {
-					// first half
-					boolean allReflect = true;
-					for (int i = 0; i < y; i++) {
-						if (!compareRows(mapPattern, i, 2 * y - i + 1)) {
-							allReflect = false;
-							horizontalReflection = 0;
-							break;
-						}
-					}
-
-					if(allReflect) {
-						break;
-					}
-				} else {
-					// second half
-					boolean allReflect = true;
-					for (int i = mapPattern.length - 1; i > y + 1; i--) {
-						if (!compareRows(mapPattern, i, 2 * y - i + 1)) {
-							allReflect = false;
-							horizontalReflection = 0;
-							break;
-						}
-					}
-
-					if(allReflect) {
-						break;
-					}
-				}
-				System.out.println("horizontal reflection found at y: " + horizontalReflection);
-			}
-		}
+		int horizontalReflection = findHorizontalReflection(mapPattern);
 
 		System.out.println();
 		final int sum = verticalReflection + (horizontalReflection * 100);
 		return sum;
 	}
 
-	private static boolean compareColumns(char[][] mapPattern, int x1, int x2) {
-		boolean allSame = true;
-		for (int y = 0; y < mapPattern.length; y++) {
-			if (mapPattern[y][x1] != mapPattern[y][x2]) {
-				allSame = false;
-				break;
+	private static int findVerticalReflection(char[][] mapPattern) {
+		int verticalReflection = 0;
+		for (int x = 0; x < mapPattern[0].length - 1; x++) {
+			final int differencesCount = compareColumns(mapPattern, x, x + 1);
+			if (differencesCount == 0) {
+				verticalReflection = x + 1;
+				boolean allReflect = true;
+				if (x + 1 < mapPattern[0].length - x) {
+					// first half
+					for (int i = 0; i < x; i++) {
+						if (compareColumns(mapPattern, i, 2 * x - i + 1) != 0) {
+							allReflect = false;
+							verticalReflection = 0;
+							break;
+						}
+					}
+
+				} else {
+					// second half
+					for (int i = mapPattern[0].length - 1; i > x + 1; i--) {
+						if (compareColumns(mapPattern, i, 2 * x - i + 1) != 0) {
+							allReflect = false;
+							verticalReflection = 0;
+							break;
+						}
+					}
+
+				}
+				System.out.println("vertical reflection found at x: " + verticalReflection);
+				if (allReflect) {
+					break;
+				}
 			}
 		}
-		return allSame;
+		return verticalReflection;
 	}
 
-	private static boolean compareRows(char[][] mapPattern, int y1, int y2) {
-		boolean allSame = true;
-		for (int x = 0; x < mapPattern[0].length; x++) {
-			if (mapPattern[y1][x] != mapPattern[y2][x]) {
-				allSame = false;
-				break; //return false;
+	private static int findAlternativeVerticalReflection(char[][] mapPattern, int originalVerticalReflection) {
+		int verticalReflection = 0;
+		for (int x = 0; x < mapPattern[0].length - 1; x++) {
+			final int differencesCount = compareColumns(mapPattern, x, x + 1);
+			verticalReflection = x + 1;
+			if (differencesCount < 2  && verticalReflection != originalVerticalReflection) {
+				int reflectionDefectCount = 0;
+				if (x + 1 < mapPattern[0].length - x) {
+					// first half
+					for (int i = 0; i <= x; i++) {
+						reflectionDefectCount += compareColumns(mapPattern, i, 2 * x - i + 1);
+						if (reflectionDefectCount > 1) {
+							verticalReflection = 0;
+							break;
+						}
+					}
+
+				} else {
+					// second half
+					for (int i = mapPattern[0].length - 1; i >= x + 1; i--) {
+						reflectionDefectCount += compareColumns(mapPattern, i, 2 * x - i + 1);
+						if (reflectionDefectCount > 1) {
+							verticalReflection = 0;
+							break;
+						}
+					}
+
+				}
+				System.out.println("alternative vertical reflection found at x: " + verticalReflection);
+				if (reflectionDefectCount == 1 && verticalReflection != originalVerticalReflection) {
+					return verticalReflection;
+				}
 			}
 		}
-		return allSame; //return true;
+		return 0;
+	}
+
+	private static int findHorizontalReflection(char[][] mapPattern) {
+		int horizontalReflection = 0;
+		for (int y = 0; y < mapPattern.length - 1; y++) {
+			final int differencesCount = compareRows(mapPattern, y, y + 1);
+
+			if (differencesCount == 0) {
+				horizontalReflection = y + 1;
+				boolean allReflect = true;
+				if (y + 1 < mapPattern.length - y) {
+					// first half
+					for (int i = 0; i < y; i++) {
+						if (compareRows(mapPattern, i, 2 * y - i + 1) != 0) {
+							allReflect = false;
+							horizontalReflection = 0;
+							break;
+						}
+					}
+
+				} else {
+					// second half
+					for (int i = mapPattern.length - 1; i > y + 1; i--) {
+						if (compareRows(mapPattern, i, 2 * y - i + 1) != 0) {
+							allReflect = false;
+							horizontalReflection = 0;
+							break;
+						}
+					}
+
+				}
+				System.out.println("horizontal reflection found at y: " + horizontalReflection);
+				if (allReflect) {
+					break;
+				}
+			}
+		}
+		return horizontalReflection;
+	}
+
+	private static int findAlternativeHorizontalReflection(char[][] mapPattern, int originalHorizontalReflection) {
+		int horizontalReflection = 0;
+		for (int y = 0; y < mapPattern.length - 1; y++) {
+			final int differencesCount = compareRows(mapPattern, y, y + 1);
+
+			horizontalReflection = y + 1;
+			if (differencesCount < 2 && horizontalReflection != originalHorizontalReflection) {
+				int intReflectionDefectCount = 0;
+				if (y + 1 < mapPattern.length - y) {
+					// first half
+					for (int i = 0; i <= y; i++) {
+						intReflectionDefectCount += compareRows(mapPattern, i, 2 * y - i + 1);
+						if (intReflectionDefectCount > 1) {
+							horizontalReflection = 0;
+							break;
+						}
+					}
+
+				} else {
+					// second half
+					for (int i = mapPattern.length - 1; i >= y + 1; i--) {
+						intReflectionDefectCount += compareRows(mapPattern, i, 2 * y - i + 1);
+						if (intReflectionDefectCount > 1) {
+							horizontalReflection = 0;
+							break;
+						}
+					}
+
+				}
+				System.out.println("alternative horizontal reflection found at y: " + horizontalReflection);
+				if (intReflectionDefectCount == 1 && horizontalReflection != originalHorizontalReflection) {
+					return horizontalReflection;
+				}
+			}
+		}
+		return 0;
+	}
+
+	private static int compareColumns(char[][] mapPattern, int x1, int x2) {
+		int differencesCount = 0;
+		for (int y = 0; y < mapPattern.length; y++) {
+			if (mapPattern[y][x1] != mapPattern[y][x2]) {
+				differencesCount++;
+			}
+		}
+		return differencesCount;
+	}
+
+	private static int compareRows(char[][] mapPattern, int y1, int y2) {
+		int differencesCount = 0;
+		for (int x = 0; x < mapPattern[0].length; x++) {
+			if (mapPattern[y1][x] != mapPattern[y2][x]) {
+				differencesCount++;
+			}
+		}
+		return differencesCount;
 	}
 }
--- src/test/java/sk/thenoen/aoc/y2023/solution/Day13Test.java
+++ src/test/java/sk/thenoen/aoc/y2023/solution/Day13Test.java
@@ -24,13 +24,35 @@ class Day13Test {
 		assertEquals(3, solution);
 	}
 
-//	@Disabled
 	@Test
 	void solvePart1() {
 		final long solution = day13.solvePart1("day13/input.txt");
 		System.out.println("solution: " + solution);
 		assertTrue(solution > 29950);
 		assertEquals(31877, solution);
+	}
+
+	@Test
+	void solvePart2Sample() {
+		final long solution = day13.solvePart2("day13/sample.txt");
+		System.out.println("solution: " + solution);
+		assertEquals(400, solution);
+	}
+
+	@Test
+	void solvePart2SampleCustom1() {
+		final long solution = day13.solvePart2("day13/sample_part2_custom1.txt");
+		System.out.println("solution: " + solution);
+		assertEquals(14, solution);
+	}
+
+//	@Disabled
+	@Test
+	void solvePart2() {
+		final long solution = day13.solvePart2("day13/input.txt");
+		System.out.println("solution: " + solution);
+		assertTrue(solution > 42700);
+		assertEquals(42996, solution);
 	}
 
 }
